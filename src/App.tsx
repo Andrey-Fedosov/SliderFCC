87f9d45ec618456693e9540af175136e89ade08d
Andrey-Fedosov <andrefedosov@gmail.com>
2024-02-21 23:42:10 +0300

    feature: try to add picture place holder

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { ImageSlider } from "./component/imageSlider/ImageSlider";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ImageSlider
+        url={"https://via.placeholder.com/600/"}
+        limit={"6"}
+        page={"1"}
+      />
     </div>
   );
 }
